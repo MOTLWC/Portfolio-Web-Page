2669d520c25b64ad68edae062be16d6f0ac9ecb6
FelixMR <felixmcneillrutter@gmail.com>
2024-09-09 15:37:51 +0100

    Added SVG element for testing

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,23 @@ function App() {
     <>
       <header>
         <h1>Felix McNeill Rutter</h1>
-        <svg width="500" height="200">
-          
+        <div className="SVGContainer">
+        <svg width="100" height="200">
+          <polygon points="20,20 80,20 80,40 40,40 40,80 60,80 60,100 40,100 40,180 20,180"/>
         </svg>
+        <svg width="100" height="200">
+          <polygon points=""/>
+        </svg>
+        <svg width="100" height="200">
+          <polygon points=""/>
+        </svg>
+        <svg width="100" height="200">
+          <polygon points=""/>
+        </svg>
+        <svg width="100" height="200">
+          <polygon points=""/>
+        </svg>
+        </div>
       </header>
 
     </>
