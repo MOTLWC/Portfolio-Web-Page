4266fa33e95fdf3acfadbbf9e5f4224fcfe1639d
FelixMR <felixmcneillrutter@gmail.com>
2024-09-09 17:07:23 +0100

    Added Basic Objects for creating different softbodies

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,29 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function Word() {
+    // Container for the letters
+    this.Letters = []
+
+  }
+
+  function Letter(points) {
+    // points should be an array of {x,y} coordinates 
+    this.points = [...points]
+    this.conections = []
+
+    this.createConnections(){
+      // gets nearest points in directions (hor, vert, diagonal) and creates a connection 
+    }
+  }
+
+  function Connection(start, end) {
+    this.start = start
+    this.end = end
+    this.length = Math.sqrt((start.x-end.x)+(start.y-end.y))
+    this.force = 0.5
+  }
+
   return (
     <>
       <header>
@@ -13,10 +36,12 @@ function App() {
           <polygon points="20,20 80,20 80,40 40,40 40,80 60,80 60,100 40,100 40,180 20,180"/>
         </svg>
         <svg width="100" height="200">
-          <polygon points=""/>
+        <polygon points="20,20 80,20 80,40 40,40"/>
+          <polygon points="20,20 40,40 40,80 20,90"/>
+          <polygon points="40,80 20,90 40,100 60,100 60,80"/>
+          <polygon points="40,100 20,90 20,180 40,180"/>
         </svg>
         <svg width="100" height="200">
-          <polygon points=""/>
         </svg>
         <svg width="100" height="200">
           <polygon points=""/>
