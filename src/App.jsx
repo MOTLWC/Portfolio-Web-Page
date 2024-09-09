373f8fb2284b4f826609df1a6ce81b770e6d1f46
FelixMR <felixmcneillrutter@gmail.com>
2024-09-09 15:23:11 +0100

    Added SVG element for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <>
-  <header>Felix McNeill Rutter</header>
+    <>
+      <header>
+        <h1>Felix McNeill Rutter</h1>
+        <svg width="500" height="200">
+          
+        </svg>
+      </header>
+
     </>
   )
 }
